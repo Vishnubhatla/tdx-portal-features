7346a0f18b3f8471b9025cab0b9dc7444cd669de
Vishnubhatla <prasanthivvs@gmail.com>
2026-06-17 11:36:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    function hideMultipleFields(ids) {
+        ids.forEach(function (id) {
+            var element = document.getElementById(id);
+            if (element) {
+                element.style.display = 'none';
+            }
+        });
+    }
+
+    // Call the function with your field IDs
+    hideMultipleFields([
+        'ctl00_ctl00_cpContent_cpContent_divModified',
+        'divCreated'
+    ]);
+
+});
+
 /*(function () {
 
   function getById(id) {
@@ -46,30 +65,9 @@
   }
 
 })();
-*/
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    function hideMultipleFields(ids) {
-        ids.forEach(function (id) {
-            var element = document.getElementById(id);
-            if (element) {
-                element.style.display = 'none';
-            }
-        });
-    }
-
-    // Call the function with your field IDs
-    hideMultipleFields([
-        'ctl00_ctl00_cpContent_cpContent_divModified',
-        'divCreated'
-    ]);
-
-});
-   
-
-
-  /*  var config = {
+ 
+ var config = {
         service: [
             'Enrollment Success / Submit a Financial Aid Web Case'
         ]
