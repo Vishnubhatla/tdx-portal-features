7bf1370453908fe470c12b611a6560c5f0066e1a
Vishnubhatla <prasanthivvs@gmail.com>
2026-07-15 12:48:08 -0400

    Update commentsRequired.js

--- commentsRequired.js
+++ commentsRequired.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var actionName = document.getElementById('hdrActionName').innerText.trim().toLowerCase();
 
-    if (actionName === 'reject') {
+    if (actionName === 'reject' or actionName === 'reject service request' ) {
         document.getElementById('ctl00_ctl00_cpContent_cpContent_txtActionComments')
                 .setAttribute('required', 'required');
     }
