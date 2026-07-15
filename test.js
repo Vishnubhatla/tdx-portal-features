0dda7d95de0fcdd502a9d5fb04aff9e4a1fe42a0
Vishnubhatla <prasanthivvs@gmail.com>
2026-07-15 13:43:03 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+var label = document.getElementById('ctl00_ctl00_cpContent_cpContent_txtActionComments');
+label.innerHTML += '<span style="color:red"> *</span>';    
+
+});
