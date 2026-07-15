e5e42ad137a7c0ebfd448921406f88cf04901195
Vishnubhatla <prasanthivvs@gmail.com>
2026-07-15 13:02:29 -0400

    Create requiredComments.js

---
+++ requiredComments.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+        document.getElementById('ctl00_ctl00_cpContent_cpContent_txtActionComments')
+                .setAttribute('required', 'required');
+});
